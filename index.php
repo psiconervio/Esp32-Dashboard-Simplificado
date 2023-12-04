91b3309260e2dc4f2db45b39a426210b78dc6d09
psiconervio <psiconervio@gmail.com>
2023-12-04 19:10:35 -0300

    muchos cambios y mejorass

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <head>
     <title>Laboratorio de Innovacion Social</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- <script src="https://kit.fontawesome.com/da4a5b6f37.js" crossorigin="anonymous"></script> -->
+    <script src="https://kit.fontawesome.com/da4a5b6f37.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <link rel="icon" href="data:,">
     <style>
@@ -105,11 +105,12 @@
       <h3>Laboratorio de Innovacion Social</h3>
     </div>
     <br>
-    <!-- MONITOREO Y CONTROL DE PANTALLAS _ -->
+
+    <!-- MONITOREO Y CONTROL DE PANTALLAS (content es el contenedor main, cards son cada uno de los bloques) -->
     <div class="content">
       <div class="cards">
         
-        <!-- == MONITOREO_ESP32_01== -->
+        <!-- ==Primer card MONITOREO_ESP32_01== izquierda -->
         <div class="card">
           <div class="card header">
             <h3 style="font-size: 1rem;">MONITOREO SENSOR ESP32_01</h3>
@@ -120,87 +121,79 @@
           <p class="temperatureColor"><span class="reading"><span id="ESP32_01_Temp"></span> &deg;C</span></p>
           <h4 class="humidityColor"><i class="fas fa-tint"></i> HUMEDAD</h4>
           <p class="humidityColor"><span class="reading"><span id="ESP32_01_Humd"></span> &percnt;</span></p>
-          
           <p class="statusreadColor"><span>Estado lectura Sensor DHT11 : </span><span id="ESP32_01_Status_Read_DHT11"></span></p>
         </div>
         
-        <!-- == Control LEDs1========================== -->
-        <!-- == MONITOREO_ESP32_02 == -->
+        <!-- ==Segundo card MONITOREO_ESP32_02 == derecha-->
         <div class="card">
           <div class="card header">
             <h3 style="font-size: 1rem;">MONITOREO SENSOR ESP32_02</h3>
           </div>
           
-          <!-- Displays the humidity and temperature values received from ESP32. *** -->
+          <!-- Muestra los valores de humedad y temperatura recibidos de ESP32. *** -->
           <h4 class="temperatureColor"><i class="fas fa-thermometer-half"></i> Temperatura</h4>
           <p class="temperatureColor"><span class="reading"><span id="ESP32_02_Temp"></span> &deg;C</span></p>
           <h4 class="humidityColor"><i class="fas fa-tint"></i> HUMEDAD</h4>
-          <p class="humidityColor"><span class="reading"><span id="ESP32_02_Humd"></span> &percnt;</span></p>
-          <!-- *********************************************************************** -->
-          
+          <p class="humidityColor"><span class="reading"><span id="ESP32_02_Humd"></span> &percnt;</span></p>          
           <p class="statusreadColor"><span>Estado Read Sensor DHT11 : </span><span id="ESP32_02_Status_Read_DHT11"></span></p>
         </div>
-        
       </div>
-            <div class="cards">
-        
+      <br>
+
+      <!-- == Segundo contenedor == -->
+      <div class="cards">   
         <!-- == MONITOREO_ESP32_02 == -->
         <div class="card">
           <div class="card header">
             <h3 style="font-size: 1rem;">MONITOREO SENSOR ESP32_03</h3>
           </div>
-          
-          <!-- Displays the humidity and temperature values received from ESP32. *** -->
+          <!-- Muestra los valores de humedad y temperatura recibidos de ESP32_03. *** -->
           <h4 class="temperatureColor"><i class="fas fa-thermometer-half"></i> Temperatura</h4>
           <p class="temperatureColor"><span class="reading"><span id="ESP32_03_Temp"></span> &deg;C</span></p>
           <h4 class="humidityColor"><i class="fas fa-tint"></i> HUMEDAD</h4>
           <p class="humidityColor"><span class="reading"><span id="ESP32_03_Humd"></span> &percnt;</span></p>
           <h4 class="anemometro_title"> <i class="fa-solid fa-gauge-simple-high"></i>Anemometro</h4>
           <p class="anemometro"><span id="ESP32_03_anemometro"></span></p>
-          <!-- *********************************************************************** -->
-          
           <p class="statusreadColor"><span>Estado Read Sensor DHT11 : </span><span id="ESP32_03_Status_Read_DHT11"></span></p>
-        </div>
-        <!-- ================================================================ -->
-        
-        <!-- ======================================================== -->
-        </div>
-
+        </div>        
+      </div>
     </div>
     
     <br>
-    
+    <footer>
     <div class="content">
       <div class="cards">
         <div class="card header" style="border-radius: 15px;">
-            <h3 style="font-size: 0.7rem;">LAST TIME RECEIVED DATA FROM ESP32 [ <span id="ESP32_01_LTRD"></span> ]</h3>
-            <button onclick="window.open('recordtable.php', '_blank');">Open Record Table</button>
+            <h3 style="font-size: 0.7rem;">ÚLTIMA VEZ RECIBIDO DATOS DE ESP32 [ <span id="ESP32_01_LTRD"></span> ]</h3>
+            <button onclick="window.open('recordtable.php', '_blank');">Abrir tabla de registros</button>
             <h3 style="font-size: 0.7rem;"></h3>
         </div>
       </div>
     </div>
-    <!-- ____________________________________________________________________ -->
-    
+    </footer>
+
     <script>
-      //PRIMER SCRIPT------------------------------------------------------------
+      //-script para actualizar valores del esp32
+      //-PONE LOS VALORES EN NULL
+      //-IMPORTANTE-Cada vez que se agrega un esp32 se debe crear otra variable xmlhttp para las demas peticiones
       document.getElementById("ESP32_01_Temp").innerHTML = "NN"; 
       document.getElementById("ESP32_01_Humd").innerHTML = "NN";
       document.getElementById("ESP32_01_Status_Read_DHT11").innerHTML = "NN";
       document.getElementById("ESP32_01_LTRD").innerHTML = "NN";
-      //------------------------------------------------------------
-      
+      document.getElementById("ESP32_01_anemometro").innerHTML ="NN";
+      document.getElementById("ESP32_03_LTRD").innerHTML = "Time : " + myObjTRES.ls_time + " | Date : " + myObjTRES  .ls_date + " (dd-mm-yyyy)";
+      //-llama a la funcion obtenerdatos y pasa el parametro esp32 que es  id del POST que recibimos de la base de datos con getdata.php
       Get_Data("esp32_01");
-      
+      //-llama a la funcion setInterval de js, para que itere la funcion myTimer cada 10seg
       setInterval(myTimer, 10000);
-      
-      //------------------------------------------------------------
+      //-mytimer ejecuta la funcion obtener datos
       function myTimer() {
-        Get_Data("esp32_01");
-      }
-      //------------------------------------------------------------
-      
+        Get_Data("esp32_01");}
+      //-get_data obtiene los datos de getdata.php y actualiza los datos en la pagina index       
       function Get_Data(id) {
+        //-cada vez que se agrega un esp32 se debe crear otra variable xmlhttp para las demas peticiones
            var xmlhttp;
+        // Verifica la compatibilidad del navegador para crear la instancia XMLHttpRequest
 				if (window.XMLHttpRequest) {
           // code for IE7+, Firefox, Chrome, Opera, Safari
           xmlhttp = new XMLHttpRequest();
@@ -208,30 +201,41 @@
           // code for IE6, IE5
           xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
         }
+        // Define la función que se ejecutará cuando cambie el estado de la solicitud
         xmlhttp.onreadystatechange = function() {
+         // Verifica si la solicitud se ha completado correctamente y el código de estado es 200 (OK)
           if (this.readyState == 4 && this.status == 200) {
+           // Parsea la respuesta JSON del servidor
             const myObj = JSON.parse(this.responseText);
             if (myObj.id == "esp32_01") {
+           // Actualiza elementos del DOM con la información recibida
               document.getElementById("ESP32_01_Temp").innerHTML = myObj.temperature;
               document.getElementById("ESP32_01_Humd").innerHTML = myObj.humidity;
               document.getElementById("ESP32_01_Status_Read_DHT11").innerHTML = myObj.status_read_sensor_dht11;
-              document.getElementById("ESP32_01_LTRD").innerHTML = "Time : " + myObj.ls_time + " | Date : " + myObj.ls_date + " (dd-mm-yyyy)";
+              document.getElementById("ESP32_03_anemometro").innerHTML = myObj.anemometro;
+
             }
           }
         };
+        // Configura la solicitud HTTP (POST) al archivo "getdata.php" de manera asíncrona
         xmlhttp.open("POST","getdata.php",true);
+       // Configura el tipo de contenido de la solicitud
         xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+       // Envía la solicitud con el parámetro "id"
         xmlhttp.send("id="+id);
 			}
- 
     </script>
 
-    <script> // segundo script------------------------------------------------------------------------ 
+    <script> // segundo script
+    //-script para actualizar valores del esp32
+    //-PONE LOS VALORES EN NULL 
     document.getElementById("ESP32_02_Temp").innerHTML = "NN"; 
     document.getElementById("ESP32_02_Humd").innerHTML = "NN";
     document.getElementById("ESP32_02_Status_Read_DHT11").innerHTML = "NN";
-    //document.getElementById("ESP32_02_LTRD").innerHTML = "NN";
+    document.getElementById("ESP32_02_LTRD").innerHTML = "NN";
+    document.getElementById("ESP32_02_anemometro").innerHTML ="NN";
     //se necesita usar otra variable xmlhttp a xmlhttpp
+    
     obtenerData("esp32_02");
     
     setInterval(myTimer1, 10000);
@@ -241,7 +245,6 @@
     }
     
     function obtenerData(id) {
-        console.log("se esta ejecutando ObtenerData")
       var xmlhttpp;
       if (window.XMLHttpRequest) {
         xmlhttpp = new XMLHttpRequest();
@@ -256,7 +259,7 @@
             document.getElementById("ESP32_02_Temp").innerHTML = myObjDOS.temperature;
             document.getElementById("ESP32_02_Humd").innerHTML = myObjDOS.humidity;
             document.getElementById("ESP32_02_Status_Read_DHT11").innerHTML = myObjDOS.status_read_sensor_dht11;
-            //document.getElementById("ESP32_02_LTRD").innerHTML = "Time : " + myObjDOS.ls_time + " | Date : " + myObjDOS  .ls_date + " (dd-mm-yyyy)";
+            document.getElementById("ESP32_03_anemometro").innerHTML = myObjDOS.anemometro;
           }
         }
       };
@@ -272,7 +275,6 @@
     document.getElementById("ESP32_03_Temp").innerHTML = "NN"; 
     document.getElementById("ESP32_03_Humd").innerHTML = "NN";
     document.getElementById("ESP32_03_Status_Read_DHT11").innerHTML = "NN";
-    //document.getElementById("ESP32_03_LTRD").innerHTML = "NN";
     document.getElementById("ESP32_03_anemometro").innerHTML ="NN";
 
     //se necesita usar otra variable xmlhttp a xmlhttpp
@@ -285,7 +287,6 @@
     }
     
     function obtenerDataa(id) {
-        console.log("se esta ejecutando ObtenerDataa TRESS")
       var xmlhttppp;
       if (window.XMLHttpRequest) {
         xmlhttppp = new XMLHttpRequest();
@@ -302,7 +303,6 @@
             document.getElementById("ESP32_03_Status_Read_DHT11").innerHTML = myObjTRES.status_read_sensor_dht11;
             //se agrego esta linea de abajo
             document.getElementById("ESP32_03_anemometro").innerHTML = myObjTRES.anemometro;
-           document.getElementById("ESP32_03_LTRD").innerHTML = "Time : " + myObjTRES.ls_time + " | Date : " + myObjTRES  .ls_date + " (dd-mm-yyyy)";
           }
         }
       };
