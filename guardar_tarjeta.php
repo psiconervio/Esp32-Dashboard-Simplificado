a9b2f93c7a88604d12191bc53fb1f6682bbbcc7f
psiconervio <psiconervio@gmail.com>
2023-12-04 21:14:59 -0300

    asd

--- guardar_tarjeta.php
+++ guardar_tarjeta.php
@@ -10,6 +10,9 @@ $esp32_id = $_POST["esp32_id"];
 $temperatura = $_POST["temperatura"];
 $humedad = $_POST["humedad"];
 $status_read_sensor_dht11 = $_POST["status_read_sensor_dht11"];
+$time = $_POST["time"];
+$date = $_POST["date"];
+$anemometro = $_POST["anemometro"];
 
 // Crear una conexión a la base de datos
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -19,12 +22,31 @@ if ($conn->connect_error) {
     die("La conexión a la base de datos ha fallado: " . $conn->connect_error);
 }
 
-// Insertar los datos en la base de datos
-$sql = "INSERT INTO tarjetas (esp32_id, temperatura, humedad, status_read_sensor_dht11) VALUES ('$esp32_id', $temperatura, $humedad, '$status_read_sensor_dht11')";
-if ($conn->query($sql) === TRUE) {
-    echo "Tarjeta guardada correctamente";
+// Crear una nueva tabla si no existe
+$tabla_nueva = "esp32_table_" . $esp32_id;
+$sql_nueva_tabla = "CREATE TABLE IF NOT EXISTS $tabla_nueva (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    temperature FLOAT(10,2) NOT NULL,
+    humidity INT NOT NULL,
+    status_read_sensor_dht11 VARCHAR(255) NOT NULL,
+    time TIME NOT NULL,
+    date DATE NOT NULL,
+    anemometro INT NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci";
+
+if ($conn->query($sql_nueva_tabla) === TRUE) {
+    echo "Tabla creada correctamente";
 } else {
-    echo "Error al guardar la tarjeta: " . $conn->error;
+    echo "Error al crear la tabla: " . $conn->error;
+}
+
+// Insertar los datos en la tabla correspondiente
+$sql_insertar_datos = "INSERT INTO $tabla_nueva (temperature, humidity, status_read_sensor_dht11, time, date, anemometro) VALUES ($temperatura, $humedad, '$status_read_sensor_dht11', '$time', '$date', $anemometro)";
+
+if ($conn->query($sql_insertar_datos) === TRUE) {
+    echo "Datos insertados correctamente";
+} else {
+    echo "Error al insertar datos: " . $conn->error;
 }
 
 // Cerrar la conexión a la base de datos
