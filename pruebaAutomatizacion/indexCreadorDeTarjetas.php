4b6bf1805fed98f8ae59600cc5210efcefe9ad7e
psiconervio <psiconervio@gmail.com>
2023-12-11 12:34:43 -0300

    asd

--- pruebaAutomatizacion/indexCreadorDeTarjetas.php
+++ pruebaAutomatizacion/indexCreadorDeTarjetas.php
@@ -1,6 +1,5 @@
 <?php
 // Funciones PHP
-
 // Conectar a la base de datos (cambiar estos valores según tu configuración)
 $servername = "localhost";
 $username = "root";
@@ -13,17 +12,35 @@ if ($conn->connect_error) {
     die("Error de conexión a la base de datos: " . $conn->connect_error);
 }
 
-function agregarTarjeta($contenido, $conn) {
+function agregarTarjeta($contenido, $contraseña, $conn) {
+
     // Añade la tarjeta a la base de datos
     $sql = "INSERT INTO tarjetas (contenido) VALUES ('$contenido')";
+   //linea para agregar contraseña a la nueva tarjeta
+    //$sql = "INSERT INTO tarjetas () VALUES ('$contraseña')";
     $conn->query($sql);
 }
 
-function quitarTarjeta($idTarjeta, $conn) {
-    // Elimina la tarjeta de la base de datos
-    $sql = "DELETE FROM tarjetas WHERE id = $idTarjeta";
-    $conn->query($sql);
-}
+      function quitarTarjeta($idTarjeta, $conn, $contraseña) {
+          // Elimina la tarjeta de la base de datos
+          $sql = "DELETE FROM tarjetas WHERE id = $idTarjeta";
+          $conn->query($sql);
+      }
+//function quitarTarjeta($idTarjeta, $conn, $contrasenaIngresada) {
+//    // Verifica la contraseña antes de proceder con la eliminación
+//    $contrasenaCorrecta = "12345"; // Reemplaza con tu contraseña real
+//
+//    if ($contrasenaIngresada == $contrasenaCorrecta) {
+//        // Elimina la tarjeta de la base de datos
+//        $sql = "DELETE FROM tarjetas WHERE id = $idTarjeta";
+//        $conn->query($sql);
+//
+//        return "Tarjeta eliminada correctamente.";
+//    } else {
+//        return "Contraseña incorrecta. No se eliminó la tarjeta.";
+//    }
+//}
+
 
 function obtenerTarjetas($conn) {
     // Obtiene todas las tarjetas almacenadas en la base de datos
@@ -39,17 +56,23 @@ function obtenerTarjetas($conn) {
 
     return $tarjetas;
 }
-
 // Procesar formulario
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['agregarTarjeta'])) {
         // Agrega tarjeta a la base de datos
         $contenidoTarjeta = "Contenido de la tarjeta"; // Puedes cambiar esto según tu necesidad
-        agregarTarjeta($contenidoTarjeta, $conn);
+        agregarTarjeta($contenidoTarjeta,$contraseña, $conn);
+
+        header("Location: {$_SERVER['PHP_SELF']}", true, 303);
+        exit();
     } elseif (isset($_POST['quitarTarjeta'])) {
         // Elimina tarjeta de la base de datos
         $idTarjeta = $_POST['quitarTarjeta'];
-        quitarTarjeta($idTarjeta, $conn);
+        quitarTarjeta($idTarjeta, $conn, $contraseña);
+
+        // Redirige después de quitar una tarjeta
+        header("Location: {$_SERVER['PHP_SELF']}", true, 303);
+        exit();
     }
 }
 ?>
@@ -58,19 +81,57 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html lang="es">
 <head>
     <link rel="stylesheet" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://kit.fontawesome.com/da4a5b6f37.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <link rel="icon" href="data:,">
-
+    <script src="s"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Botones Tarjetas</title>
 </head>
-<body>
+<script> //funcion para contraseña tarjeta
+        // funcion creartarjetas() {
+        //const = contraseñaCard = prompt("ingrese una contraseña para esta tarjeta");  }
+</script>
+<script>
+//    function solicitarContrasena(idTarjeta) {
+//        // Solicitar contraseña mediante un prompt
+//        const contrasenaIngresada = prompt("Ingrese la contraseña para quitar la tarjeta:");
+//
+//        // Verificar si se ingresó una contraseña
+//        if (contrasenaIngresada !== null) {
+//            // Agregar la contraseña como un campo oculto al formulario
+//            const formulario = document.createElement('form');
+//            formulario.method = 'post';
+//            formulario.action = ''; // Establece la acción del formulario
+//
+//            const inputContrasena = document.createElement('input');
+//            inputContrasena.type = 'hidden';
+//            inputContrasena.name = 'contrasena';
+//            inputContrasena.value = contrasenaIngresada;
+//
+//            formulario.appendChild(inputContrasena);
+//
+//            // Agregar el ID de la tarjeta como un campo oculto al formulario
+//            const inputIdTarjeta = document.createElement('input');
+//            inputIdTarjeta.type = 'hidden';
+//            inputIdTarjeta.name = 'quitarTarjetaId';
+//            inputIdTarjeta.value = idTarjeta;
+//
+//            formulario.appendChild(inputIdTarjeta);
+//
+//            // Adjuntar el formulario al cuerpo del documento y enviarlo
+//            document.body.appendChild(formulario);
+//            formulario.submit();
+//        }
+//    }
+</script>
 
+<body>
 <!-- Formulario con botones -->
 <form method="post">
-    <button type="submit" name="agregarTarjeta">Agregar Tarjeta</button>
+    <button id="btnAgregarTarjeta" type="submit" name="agregarTarjeta">Agregar Tarjeta</button>
 </form>
 <div class="content">
     <div class="cards">
@@ -79,7 +140,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 $tarjetas = obtenerTarjetas($conn);
 foreach ($tarjetas as $tarjeta) {
     echo '<div class="card">';
-    echo '<div class="card-header">';
+    echo '<div class="card header">';
     echo '<h3 style="font-size: 1rem;">MONITOREO SENSOR ESP32_' . $tarjeta['id'] . '</h3>';
     echo '</div>';
     echo '<h4 class="temperatureColor"><i class="fas fa-thermometer-half"></i> TEMPERATURA</h4>';
@@ -93,15 +154,11 @@ foreach ($tarjetas as $tarjeta) {
     echo '<h4 class="pluviometro_title"><i class="fa-solid fa-cloud-rain"></i> CAUDAL DE LLUVIA </h4>';
     echo '<p class="pluviometro"><span class="reading"><span id="ESP32_' . $tarjeta['id'] . '_pluviometro"></span> ml</span></p>';
     echo '<p class="statusreadColor"><span>Estado Read Sensor DHT11 : </span><span id="ESP32_' . $tarjeta['id'] . '_Status_Read_DHT11"></span></p>';
-    echo '<form method="post"><button type="submit" name="quitarTarjeta" value="' . $tarjeta['id'] . '">Quitar Tarjeta</button></form>';
+    echo '<form method="post"><button type="submit" name="quitarTarjeta" value="' . $tarjeta['id'] . '" onclick="solicitarContrasena(' . $tarjeta['id'] . ')">Quitar Tarjeta</button></form>';
     echo '</div>';
 }
 ?>
-
-<?php
-
-?>
-    </div>
+</div>
 </div>
 </body>
 </html>
