4f3cb6224dfe87d2a9f6262e7c5ef5d542d130a6
psiconervio <psiconervio@gmail.com>
2024-02-25 22:24:40 -0300

    asd

--- indexNUEVODISEÑO.php
+++ indexNUEVODISEÑO.php
@@ -9,6 +9,42 @@
       $('body').removeClass('hidden');
     }
   </script>
+  <!--espacio de script medidor uv-->
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+        ['Task', 'Hours per Day'],
+          ['transparent', 50],
+          ['low', 10],
+          ['medium', 10],
+          ['higt', 10],
+          ['very high', 10],
+          ['extremo', 10]
+        ]);
+
+        var options = {
+          legend: 'none',
+          pieSliceText: 'none',
+          pieStartAngle: 90,
+          slices: {
+            0: { color: 'transparent' },
+            1: { color: '#4fb400' },
+            2: { color: '#f8b600' },
+            3: { color: '#f85900' },
+            4: { color: '#d81f1d' },
+            5: { color: '#998cff' },
+
+          }
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+        chart.draw(data, options);
+      }
+    </script>
+    <!--termina-->
   <script src="pruebaAutomatizacion\apiclimaa.js"></script>
   <title>Laboratorio de Innovacion Social</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -72,7 +108,12 @@ fetch("https://api.openuv.io/api/v1/uv?lat=-28.51&lng=-65.82&alt=100&dt=", reque
             <p>Rafaga de viento <span id="rafagadeviento"></span> km/h </p>
             
         </div>
-
+        <div id="piechart" style="width: 100px; height: 100x;"></div>
+        <div class="contenedorItem">
+              <span class="reading"><i class="fa-solid fa-arrow-down-short-wide" aria-hidden="true"></i> <span
+                  id="presion"></span> hPA</span>
+              <p class="pluviometro_title"></i> Presion Atmosferica<br> </p>
+            </div>
         <div class="detalles">
         <h2>Detalles</h2>
         <br>
