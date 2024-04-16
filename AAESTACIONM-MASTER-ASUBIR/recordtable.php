f977174be6e0ab763d92c66c2e46d8f5081157ef
psiconervio <psiconervio@gmail.com>
2024-04-15 21:29:32 -0300

    asd

--- AAESTACIONM-MASTER-ASUBIR/recordtable.php
+++ AAESTACIONM-MASTER-ASUBIR/recordtable.php
@@ -182,50 +182,39 @@
       </select>
       <button class="button" id="btn_apply" onclick="apply_Number_of_Rows()">Aplicar</button>
     </div>
-
     <br>
-
-
     <script>
       //------------------------------------------------------------
       var current_page = 1;
       var records_per_page = 10;
       var l = document.getElementById("table_id").rows.length
-      //------------------------------------------------------------
       
       //------------------------------------------------------------
-      function apply_Number_of_Rows() {
+            function apply_Number_of_Rows() {
         var x = document.getElementById("number_of_rows").value;
         records_per_page = x;
         changePage(current_page);
       }
       //------------------------------------------------------------
-      
-      //------------------------------------------------------------
-      function prevPage() {
+            function prevPage() {
         if (current_page > 1) {
             current_page--;
             changePage(current_page);
         }
       }
       //------------------------------------------------------------
-      
-      //------------------------------------------------------------
       function nextPage() {
         if (current_page < numPages()) {
             current_page++;
             changePage(current_page);
         }
-      }
-      //------------------------------------------------------------
-      
+      }      
       //------------------------------------------------------------
       function changePage(page) {
         var btn_next = document.getElementById("btn_next");
         var btn_prev = document.getElementById("btn_prev");
         var listing_table = document.getElementById("table_id");
         var page_span = document.getElementById("page");
-       
         // Validate page
         if (page < 1) page = 1;
         if (page > numPages()) page = numPages();
@@ -262,14 +251,11 @@
         } else {
           btn_next.disabled = false;
         }
-      }
-      //------------------------------------------------------------
-      
+      }      
       //------------------------------------------------------------
       function numPages() {
         return Math.ceil((l - 1) / records_per_page);
       }
-      //------------------------------------------------------------
       
       //------------------------------------------------------------
       window.onload = function() {
@@ -278,11 +264,12 @@
         changePage(current_page);
       };
       //------------------------------------------------------------
+      console.log(windows.onload);
     </script>
         <h1>grafico</h1>
         <canvas id="myChart"></canvas>
     <script>
-      
+   
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'line',
@@ -308,9 +295,6 @@ var myChart = new Chart(ctx, {
         }
     }
 });
-
-
 </script>
-
   </body>
 </html>
